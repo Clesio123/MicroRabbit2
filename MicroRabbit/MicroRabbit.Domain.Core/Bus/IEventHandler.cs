f28f2c805c435598dd7eeeaf992d73de0e935e31
Clesio Manave <clesio.s.manave@gmail.com>
2021-06-18 00:07:28 +0200

    Inicial Commit of MicroRabbit Domain Core Project

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MicroRabbit.Domain.Core.Events;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+   public class IEventHandler
+        where TEvent : Event
+    {
+
+        Task Handle(TEvent @event);
+    }
+
+    public interface EventHandler
+    {
+
+    }
+}
